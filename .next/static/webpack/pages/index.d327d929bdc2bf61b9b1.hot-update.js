d24fde636d42ba6b27ae840ca0f03d43f1a452c4
DreMind5000 <89564302+DreMind5000@users.noreply.github.com>
2022-02-14 10:42:27 -0500

    update

---
+++ .next/static/webpack/pages/index.d327d929bdc2bf61b9b1.hot-update.js
@@ -0,0 +1,16 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./public/images/Cover3.PNG":
+/*!**********************************!*\
+  !*** ./public/images/Cover3.PNG ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({"src":"/_next/static/image/public/images/Cover3.6463830399c2422f2710a9de11f60c0c.PNG","height":380,"width":402});
+
+/***/ })
+
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svcGFnZXMvaW5kZXguZDMyN2Q5MjliZGMyYmY2MWI5YjEuaG90LXVwZGF0ZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsK0RBQWUsQ0FBQywrR0FBK0ciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL2ltYWdlcy9Db3ZlcjMuUE5HIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcInNyY1wiOlwiL19uZXh0L3N0YXRpYy9pbWFnZS9wdWJsaWMvaW1hZ2VzL0NvdmVyMy42NDYzODMwMzk5YzI0MjJmMjcxMGE5ZGUxMWY2MGMwYy5QTkdcIixcImhlaWdodFwiOjM4MCxcIndpZHRoXCI6NDAyfTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
